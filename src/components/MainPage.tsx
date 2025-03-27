6eecf2b204aa5f0e13e4719ceff0af40baa9b4d5
Teodora <vestale.teodora00@gmail.com>
2025-03-27 19:10:31 +0200

    feat: footer changes

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,13 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import { fetchSkips } from '../api';
 import Content from './Content';
 import { Skip } from '../types/SkipType';
+import Footer from './Footer';
 
 const MainPage = () => {
     const [skips, setSkips] = useState<Skip[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
+    const [selectedSkip, setSelectedSkip] = useState<Skip | null>(null);
 
     useEffect(() => {
         getSkips();
@@ -26,12 +28,19 @@ const MainPage = () => {
     }
 
     return (
-        <div className="container">
-            <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
-                <Header />
+        <>
+            <div className="container">
+                <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50">
+                    <Header />
+                </div>
+                <Content skips={skips} loading={loading} error={error} setSelectedSkip={setSelectedSkip} />
             </div>
-            <Content skips={skips} loading={loading} error={error} />
-        </div>
+            {selectedSkip && (
+                <div>
+                    <Footer selectedSkip={selectedSkip} />
+                </div>
+            )}
+        </>
     );
 };
 
